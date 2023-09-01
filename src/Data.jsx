c33a38126dc418846e206a6a5a7217d8ff800ad6
nurainande <nurainande@gmail.com>
2023-09-01 10:30:57 -0700

    first commit

---
+++ src/Data.jsx
@@ -0,0 +1,22 @@
+export const userList = [
+  {
+    name: "Bello Ahmed",
+    email: "bl@gmail.com",
+    id: 1,
+  },
+  {
+    name: "John Peter",
+    email: "jp@gmail.com",
+    id: 2,
+  },
+  {
+    name: "Joseph Mary",
+    email: "jm@gmail.com",
+    id: 3,
+  },
+  {
+    name: "Nafisat Bamidele",
+    email: "nb@gmail.com",
+    id: 3,
+  },
+];
